6334662e92a41afc830f98f250977c546d07c391
tktknty <tktknty310@gmail.com>
2023-10-04 00:25:07 +0900

    add: test1

--- App.tsx
+++ App.tsx
@@ -1,22 +1,8 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { AppBar } from './src/components/AppBar';
-import { CircleButton } from './src/components/CircleButton';
-import { MemoList } from './src/components/MemoList';
+import { MemoCreateScreen } from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AppBar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
-    </View>
+    <MemoCreateScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F4F8',
-  },
-});
